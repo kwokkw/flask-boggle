3cb659ec47187733a65c5e6f628f848acf0ea9d5
kwokkw <68409369+kwokkw@users.noreply.github.com>
2024-06-21 06:32:41 -1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from boggle import Boggle
-from flask import Flask, render_template, session, request, jsonify, redirect
+from flask import (
+    Flask,
+    render_template,
+    session,
+    request,
+    redirect,
+    flash,
+    jsonify,
+    json,
+)
 from flask_debugtoolbar import DebugToolbarExtension
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "play"
+app.debug = True
 
 # Install flask debug toolbar
 debug = DebugToolbarExtension(app)
@@ -20,8 +30,18 @@ def home_page():
     return render_template("index.html", game_board=game_board)
 
 
+# TODO
 @app.route("/submit-guess", methods=["POST"])
-def submit():
-    #     # take the form value and check if it is a valid word in the dictionary using the words variable in your app.py
+def submit_guess():
+    # take the form value and check if it is a valid word in the dictionary using the words variable in your app.py
+    guess = request.json["guess"]
+    game_board = session["game_board"]
+    response = boggle_game.check_valid_word(game_board, guess)
 
-    return render_template("submit-guess.html")
+    # UNABLE TO SEE PRINT STATEMENT IN GIT BASH - do not understand the behavior
+    print("***********************")
+    print(guess)
+    print("***********************")
+
+    # `jsonify` converts Python data into a JSON format that can be sent and understood by JavaScript.
+    return jsonify({"result": response})
